5c77b51c40987073d08cedbe5328801b1765abe8
Oleg Sokolov <turgenoid@gmail.com>
2019-06-03 18:12:18 +0300

    Исправление ошибки выхода за границы массива

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class Bot01:
 		if len(get_result) > 0:
 			last_update = get_result[-1]
 		else:
-			last_update = get_result[len(get_result)]
+			last_update = get_result[len(get_result)-1]
 
 		return last_update
 
